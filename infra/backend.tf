c6bf1605262dada4486381a55353ba127c8db6f2
Valeriia Lapytska <lerkasan@gmail.com>
2024-07-16 14:22:57 -0400

    destroy old infra

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region  = "us-east-1"
     bucket  = "greencity-terraform-state"
-    key     = "production/terraform-infra.tfstate"
+    key     = "stage/terraform-infra.tfstate"
     encrypt = true
     acl     = "private"
   }
--- k8s_addons/backend.tf
+++ k8s_addons/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region  = "us-east-1"
     bucket  = "greencity-terraform-state"
-    key     = "production/terraform-greencity-argocd.tfstate"
+    key     = "stage/terraform-greencity-argocd.tfstate"
     encrypt = true
     acl     = "private"
   }
