d7b061e36b03c6e7f004f773f2c9eb86bb65e80b
Valeriia Lapytska <lerkasan@gmail.com>
2024-07-16 11:45:19 -0400

    switched to AWS_ACCESS_KEY credentials

--- k8s_addons/backend.tf
+++ k8s_addons/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region  = "us-east-1"
     bucket  = "greencity-terraform-state"
-    key     = "stage/terraform-greencity-argocd.tfstate"
+    key     = "production/terraform-greencity-argocd.tfstate"
     encrypt = true
     acl     = "private"
   }
