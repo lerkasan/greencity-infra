93fc626ced2e5fefb10f220e085f611aee75b7e8
Valeriia Lapytska <lerkasan@gmail.com>
2024-07-16 14:39:18 -0400

    once more

--- infra/providers.tf
+++ infra/providers.tf
@@ -26,22 +26,22 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "kubernetes" {
-  #   host                   = module.custom_eks.cluster_endpoint # data.aws_eks_cluster.default.endpoint
-  #   cluster_ca_certificate = base64decode(module.custom_eks.cluster_certificate_authority_data)
-  #   host                   = data.aws_eks_cluster.default.endpoint
-  host                   = module.custom_eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.custom_eks.certificate_authority_data)
-  #   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  #   load_config_file       = false
+# provider "kubernetes" {
+#   #   host                   = module.custom_eks.cluster_endpoint # data.aws_eks_cluster.default.endpoint
+#   #   cluster_ca_certificate = base64decode(module.custom_eks.cluster_certificate_authority_data)
+#   #   host                   = data.aws_eks_cluster.default.endpoint
+#   host                   = module.custom_eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.custom_eks.certificate_authority_data)
+#   #   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+#   #   load_config_file       = false
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name, "--region", var.aws_region]
-    # args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]  #, "--region", var.aws_region] # module.custom_eks.cluster_name] # data.aws_eks_cluster.default.id] #, "--profile", "eks-admin" ]
-    command = "aws"
-  }
-}
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name, "--region", var.aws_region]
+#     # args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]  #, "--region", var.aws_region] # module.custom_eks.cluster_name] # data.aws_eks_cluster.default.id] #, "--profile", "eks-admin" ]
+#     command = "aws"
+#   }
+# }
 
 # provider "kubectl" {
 #   host                   = data.aws_eks_cluster.default.endpoint
@@ -56,21 +56,21 @@ provider "kubernetes" {
 #     }
 # }
 
-provider "helm" {
-  kubernetes {
-    # config_path = "~/.kube/config"
-    # host                   = module.custom_eks.cluster_endpoint # data.aws_eks_cluster.default.endpoint
-    # cluster_ca_certificate = base64decode(module.custom_eks.cluster_certificate_authority_data)
-    # host                   = data.aws_eks_cluster.default.endpoint
-    host = module.custom_eks.cluster_endpoint
-    # cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-    cluster_ca_certificate = base64decode(module.custom_eks.certificate_authority_data)
+# provider "helm" {
+#   kubernetes {
+#     # config_path = "~/.kube/config"
+#     # host                   = module.custom_eks.cluster_endpoint # data.aws_eks_cluster.default.endpoint
+#     # cluster_ca_certificate = base64decode(module.custom_eks.cluster_certificate_authority_data)
+#     # host                   = data.aws_eks_cluster.default.endpoint
+#     host = module.custom_eks.cluster_endpoint
+#     # cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+#     cluster_ca_certificate = base64decode(module.custom_eks.certificate_authority_data)
 
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      #   args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id, "--region", var.aws_region] # module.custom_eks.cluster_name] #var.eks_cluster_name] # data.aws_eks_cluster.default.id] #, "--profile", "eks-admin"]
-      args    = ["eks", "get-token", "--cluster-name", var.eks_cluster_name, "--region", var.aws_region] # module.custom_eks.cluster_name] #var.eks_cluster_name] # data.aws_eks_cluster.default.id] #, "--profile", "eks-admin"]
-      command = "aws"
-    }
-  }
-}
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       #   args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id, "--region", var.aws_region] # module.custom_eks.cluster_name] #var.eks_cluster_name] # data.aws_eks_cluster.default.id] #, "--profile", "eks-admin"]
+#       args    = ["eks", "get-token", "--cluster-name", var.eks_cluster_name, "--region", var.aws_region] # module.custom_eks.cluster_name] #var.eks_cluster_name] # data.aws_eks_cluster.default.id] #, "--profile", "eks-admin"]
+#       command = "aws"
+#     }
+#   }
+# }
